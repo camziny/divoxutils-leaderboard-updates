38389ea7053dbeafa502e07667ee42bdbed12842
camziny <cameronziny@gmail.com>
2025-06-21 09:53:50 -0400

    - update batching and cron

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'divoxutils-automation',
+    script: 'index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '100M',
+    env_file: '.env',
+    env: {
+      NODE_ENV: 'production'
+    },
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    time: true
+  }]
+} 
